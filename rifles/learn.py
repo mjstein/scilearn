63d0c8160725dea6f685ee2fd94d28560e18b357
Mike Stein <michael.stein@sky.uk>
2018-02-21 20:51:11 +0000

    add some more goodies

--- rifles/learn.py
+++ rifles/learn.py
@@ -1,12 +1,18 @@
 from sklearn import tree
 from type.gun import *
 
-mg1 = (Gun(30,100).get(),MACHINE_GUN)
-mg2 = (Gun(300,120).get(),MACHINE_GUN)
-rifle1 = (Gun(1,80).get(),RIFLE)
-smg1=(Gun(40,30).get(),SMG)
+mg1 = (Gun(30,9,1000).get(),MACHINE_GUN)
+mg2 = (Gun(300,10,2000).get(),MACHINE_GUN)
+rifle1 = (Gun(1,4,20).get(),RIFLE)
+rifle2 = (Gun(6,5,24).get(),RIFLE)
+smg1=(Gun(40,2,300).get(),SMG)
+at1=(Gun(1,12,1).get(),AT)
+at2=(Gun(2,15,2).get(),AT)
+at3=(Gun(1,7,1).get(),AT)
+p1=(Gun(30,1,24).get(),PISTOL)
+p2=(Gun(28,2,22).get(),PISTOL)
 
-gun_arr=[mg1,mg2,rifle1,smg1]
+gun_arr=[mg1,mg2,rifle1,rifle2,smg1,at1,at2,at3,p1,p2]
 features=[]
 labels=[]
 for gun,gun_type in gun_arr:
@@ -25,6 +31,6 @@ with open('dogs.dot', 'w') as dotfile:
     tree.export_graphviz(
             classifier,
             dotfile,
-            feature_names=['Fire rate(rpm)', 'Weight(kg)'],
-            class_names=['Machine_gun','Rifle', 'SMG'])
+            feature_names=['Fire rate(rpm)', 'Weight(kg)','Magazine capacity(rounds)'],
+            class_names=['Machine gun','Rifle', 'SMG','Anti Tank','Pistol'])
 
--- rifles/type/gun.py
+++ rifles/type/gun.py
@@ -1,25 +1,33 @@
 MACHINE_GUN=0
 RIFLE=1
 SMG=2
+AT=3
+PISTOL=4
 def get_input():
     fire_rate = input("Gun fire rate in rpm: ")
     weight = input("Gun weight in kg: ")
-    return Gun(int(fire_rate),int(weight))
+    capacity = input("Gun capacity in rounds: ")
+    return Gun(int(fire_rate),int(weight),int(capacity))
 def i_am(result):
     if (result[0] == MACHINE_GUN):
-        print("I am a machine gun")
+        print("I am a Machine gun")
     elif (result[0]) == RIFLE:
-        print("I am a rifle")
+        print("I am a Rifle")
     elif (result[0]) == SMG:
         print("I am a SMG")
+    elif (result[0]) == AT:
+        print("I am an Anti Tank Gun")
+    elif (result[0]) == PISTOL:
+        print("I am a Pistol")
     else:
         print("I am unknown!")
 
 class Gun:
- def __init__(self, fire_rate, weight):
+ def __init__(self, fire_rate, weight,capacity):
      self.fire_rate = fire_rate
      self.weight = weight
+     self.capacity = capacity
  def get(self):
-     return [self.fire_rate,self.weight]
+     return [self.fire_rate,self.weight,self.capacity]
  def print(self):
-     print("I am a gun with fire rate of %s with a weight of %s" % (self.fire_rate, self.weight))
+     print("I am a gun with fire rate of %s with a weight of %s and an ammunition capacity of %s rounds" % (self.fire_rate, self.weight, self.capacity))
